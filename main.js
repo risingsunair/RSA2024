3189b6822b24396749053995ade014ca08fe3a71
daniellasilva.design <daniellasilva.design@gmail.com>
2024-06-16 20:22:57 -0600

    first commit

---
+++ main.js
@@ -0,0 +1,66 @@
+function muestra_oculta(id){
+    let div = document.getElementById(id);
+    if (div.style.display == "none"){
+        div.style.display = "flex";
+    }
+    else {
+        div.style.display = "none";
+    }
+}
+
+/*Mapa*/
+"mapInfo": {
+        "title": "You can find us",
+        "text": "Our team is well-versed in all aspects of air conditioning systems, including AC installation, repair, and maintenance, as well as heating services for optimal comfort throughout the year. With nearly 30 years of experience in the HVAC industry, Rising Sun Air proudly serves:",
+        "mapUrl": "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3381322.8777162437!2d-114.57616359449354!3d34.14447130493246!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x872b134b1571354f%3A0x39ca9357f26ea536!2sRising%20Sun%20Air%2C%20Inc.!5e0!3m2!1ses-419!2smx!4v1680071191196!5m2!1ses-419!2smx",
+        "ubications": [
+            {
+                "mapUrl": "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d425345.6116054047!2d-112.45468914041219!3d33.60497778647487!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x872b12ed50a179cb%3A0x8c69c7f8354a1bac!2sPhoenix%2C%20Arizona%2C%20EE.%20UU.!5e0!3m2!1ses!2smx!4v1682757298221!5m2!1ses!2smx",
+                "city": "Phoenix, Arizona"
+            },
+            {
+                "mapUrl": "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d213218.47486592544!2d-112.31933355!3d33.38310815!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x872b2ff4d3b044f3%3A0x45354c19a9434f0f!2sAvondale%2C%20Arizona%2C%20EE.%20UU.!5e0!3m2!1ses!2smx!4v1682364853856!5m2!1ses!2smx",
+                "city": "Avondale"
+            },
+            {
+                "mapUrl": "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d213294.43827238827!2d-112.418516!3d33.35211795!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x872b3a65dfa1b71d%3A0x67b27a8f85f953fa!2sGoodyear%2C%20Arizona%2C%20EE.%20UU.!5e0!3m2!1ses!2smx!4v1682364715627!5m2!1ses!2smx",
+                "city": "Goodyear"
+            },
+            {
+                "mapUrl": "https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d426588.1995530783!2d-112.7481857!3d33.3522561!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x872b3e33958cead9%3A0x40f5d758683ece0e!2sTolleson%2C%20Arizona%2C%20EE.%20UU.!5e0!3m2!1ses!2smx!4v1682364875804!5m2!1ses!2smx",
+                "city": "Tolleson"
+            },
+            {
+                "mapUrl": "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d106732.04111236219!2d-111.86385295!3d33.2828175!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x872a55595c22b693%3A0x386ce373138cfdf0!2sChandler%2C%20Arizona%2C%20EE.%20UU.!5e0!3m2!1ses!2smx!4v1682364899271!5m2!1ses!2smx",
+                "city": "Chandler"
+            },
+            {
+                "mapUrl": "https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d213433.3209854523!2d-111.9287774!3d33.2953931!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x872ba90de045ae43%3A0x6453ae977fd9ef7e!2sGilbert%2C%20Arizona%2C%20EE.%20UU.!5e0!3m2!1ses!2smx!4v1682364927357!5m2!1ses!2smx",
+                "city": "Gilbert"
+            },
+            {
+                "mapUrl": "https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d213433.90937017035!2d-112.0593328!3d33.2951526!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x872b23272d679d37%3A0x9f14430b838d3b54!2sGlendale%2C%20Arizona%2C%20EE.%20UU.!5e0!3m2!1ses!2smx!4v1682364947052!5m2!1ses!2smx",
+                "city": "Glendale"
+            },
+            {
+                "mapUrl": "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d213188.08253530166!2d-111.73776760000001!3d33.39549995!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x872ba7c2c0c592c1%3A0xd10fc215694e771b!2sMesa%2C%20Arizona%2C%20EE.%20UU.!5e0!3m2!1ses!2smx!4v1682364968298!5m2!1ses!2smx",
+                "city": "Mesa"
+            },
+            {
+                "mapUrl": "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d212306.97931119948!2d-112.32505644999999!3d33.7530019!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x872b5ae6de3cc901%3A0xd3dfa520c130189d!2sPeoria%2C%20Arizona%2C%20EE.%20UU.!5e0!3m2!1ses!2smx!4v1682364994098!5m2!1ses!2smx",
+                "city": "Peoria"
+            },
+            {
+                "mapUrl": "https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d424613.300450687!2d-112.6547258!3d33.7531348!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x872b08d9a7772c97%3A0x26d2e412188fe618!2sScottsdale%2C%20Arizona%2C%20EE.%20UU.!5e0!3m2!1ses!2smx!4v1682365016419!5m2!1ses!2smx",
+                "city": "Scottsdale"
+            },
+            {
+                "mapUrl": "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d106598.4289583495!2d-111.92810105000001!3d33.3919224!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x872b0898a6aa80e3%3A0xa2dd4aad392cb41f!2sTempe%2C%20Arizona%2C%20EE.%20UU.!5e0!3m2!1ses!2smx!4v1682365036380!5m2!1ses!2smx",
+                "city": "Tempe"
+            },
+            {
+                "mapUrl": "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d106804.42038438516!2d-111.6073649!3d33.22358175!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x872a4c65d79a826d%3A0x222c5c0cfe71e0e0!2sQueen%20Creek%2C%20Arizona%2C%20EE.%20UU.!5e0!3m2!1ses!2smx!4v1682365058163!5m2!1ses!2smx",
+                "city": "Queen Creek"
+            }
+        ]
+    }
